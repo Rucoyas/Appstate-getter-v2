26faf47fbb9279964498a0bfacd5229cd075575d
jonellcc <jonellmagallanescc102005@gmail.com>
2024-06-21 11:36:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
